74ca45063c1e8173f1fede56049ab9902f2326aa
Luke Deen Taylor <luke@deentaylor.com>
2017-05-13 16:38:09 -0400

    Move sentences to separate file

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,30 +1,13 @@
 const Markov = require('../..');
 const runner = require('../runner');
+const fs = require('fs');
 
-const sentences = [
-  'The boy plays',
-  'Jack eats',
-  'Jack quickly eats',
-  'Jack quickly eats carrots',
-  'Jack quickly eats fresh carrots at home',
-  'Sara sits',
-  'The girl pets the cat',
-  'I love apples',
-  'Bill kicks the ball',
-  'Bill kicks the red ball hard every day',
-  'Lisa is pretty',
-  'They are nice',
-  'I am sad',
-  'Maria laughs loudly',
-  'The dog jumps high',
-  'Apples are everywhere',
-  'I am the teacher',
-  'Jon is a carpenter',
-  'The boy is a student',
-  'Lisa looks pretty tonight',
-  'Green apples are everywhere',
-  'Jon is a smart student at school',
-];
+const sentences = fs.readFileSync(`${__dirname}/sentences.txt`)
+  .toString()
+  .toLowerCase()
+  .split('\n')             // Split by lines
+  .filter(s => s)          // Remove empty elements
+  .map(s => s.split(' ')); // Split each element by spaces
 
-const m = new Markov(sentences.map(s => s.toLowerCase().split(' ')));
+const m = new Markov(sentences);
 runner(m);
