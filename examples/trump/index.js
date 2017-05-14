b4d469d050aa2c729c572499b74b7077cfa0c1cd
Luke Deen Taylor <luke@deentaylor.com>
2017-05-14 00:03:27 -0400

    Add trump tweets example

---
+++ examples/trump/index.js
@@ -0,0 +1,47 @@
+const Markov = require('../..');
+const runner = require('../runner');
+const fs = require('fs');
+const Twit = require('twit');
+const bigInt = require('big-integer');
+
+const keys = JSON.parse(fs.readFileSync(`${__dirname}/keys.json`).toString());
+
+const t = new Twit({
+  consumer_key: keys.consumer_key,
+  consumer_secret: keys.consumer_secret,
+  access_token: keys.access_token,
+  access_token_secret: keys.access_token_secret,
+  timeout_ms: 5 * 1000,
+});
+
+const tweets = [];
+const numTweets = 3200;
+
+
+function getTweets(maxId, iteration, callback) {
+  console.log(`Fetching tweets page ${iteration}`);
+
+  // Get 200 more tweets
+  t.get('statuses/user_timeline', { screen_name: 'realDonaldTrump', count: 200, max_id: maxId })
+    .then((result) => {
+      const data = result.data;
+      // Add tweets to the list
+      tweets.push(...data.map(tweet => tweet.text));
+      // Max ID is one less than the ID of the least recent tweet returned by this call. (see
+      // https://dev.twitter.com/rest/public/timelines)
+      if (iteration * 200 < numTweets) {
+        // Recursive call to get next page
+        getTweets(
+          bigInt(data[data.length - 1].id_str).subtract(1).toString(),
+          iteration + 1,
+          callback,
+        );
+      } else callback(); // If we're done adding things to tweets, run the callback
+    });
+}
+
+getTweets(undefined, 1, () => {
+  const data = tweets.map(tweet => tweet.toLowerCase().split(' ').filter(n => !n.includes('t.co/')));
+  const m = new Markov(data);
+  runner(m, 100);
+});
