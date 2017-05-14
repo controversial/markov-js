2e0eaa4882c20d70553cb757a577cc5bdd440035
Luke Deen Taylor <luke@deentaylor.com>
2017-05-14 00:04:07 -0400

    Drastically simplify runner

--- examples/runner.js
+++ examples/runner.js
@@ -1,28 +1,6 @@
 const chalk = require('chalk');
-const Table = require('cli-table');
 
-const chars = {
-  top: '',
-  'top-mid': '',
-  'top-left': '',
-  'top-right': '',
-  bottom: '',
-  'bottom-mid': '',
-  'bottom-left': '',
-  'bottom-right': '',
-  left: '',
-  'left-mid': '',
-  mid: '',
-  'mid-mid': '',
-  right: '',
-  'right-mid': '',
-  middle: ' ',
-};
-
-
-function multiRunner(markov, iterations) {
-  const table = new Table({ chars, style: { 'padding-left': 0, 'padding-right': 0 } });
-
+module.exports = function runner(markov, iterations = 10) {
   for (let i = 1; i <= iterations; i += 1) {
     // Run
     const startTime = new Date();
@@ -30,25 +8,11 @@ function multiRunner(markov, iterations) {
     const timeElapsed = ((new Date() - startTime) / 1000).toFixed(3);
 
     // Record
-    table.push([
-      chalk.yellow(i),
-      output,
+    console.log(
+      '\n',
+      chalk.yellow(`${i}.${' '.repeat(iterations.toString().length - i.toString().length)}`),
       chalk.blue(`${timeElapsed}s`),
-    ]);
+      output,
+    );
   }
-
-  // Output
-  console.log(table.toString());
-}
-
-function singleRunner(markov) {
-  const startTime = new Date();
-  const output = markov.get().join(' ');
-  const timeElapsed = ((new Date() - startTime) / 1000).toFixed(3);
-
-  console.log(chalk.blue(timeElapsed), output);
-}
-
-module.exports = function runner(markov, iterations = 10) {
-  (iterations > 1 ? multiRunner : singleRunner)(markov, iterations);
 };
