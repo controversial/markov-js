73491ea38904429e5b0ebd90f2d048828997afa1
Luke Taylor <luke@deentaylor.com>
2017-05-24 20:12:00 +0000

    Include all of Alice in Wonderland

--- examples/wonderland/index.js
+++ examples/wonderland/index.js
@@ -3,11 +3,11 @@ const runner = require('../runner');
 
 const fs = require('fs');
 
-const sentences = fs.readFileSync(`${__dirname}/chapter1.txt`)
+const sentences = fs.readFileSync(`${__dirname}/wonderland.txt`)
   .toString()
-  .toLowerCase()
-  .replace(/\n/g, ' ')      // Remove line breaks
-  .split('.')              // Split into sentences by periods
+  .split('THE END')[0]     // Exclude gutenberg license
+  .replace(/\s+/g, ' ')    // Normalize all whitespace to a single space
+  .split('.')              // Split into sentences by periods 
   .map(s => s.split(' ')); // Split each element by spaces
 
 const m = new Markov(sentences);
