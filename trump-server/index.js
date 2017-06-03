f5e0f1dcd3ff96e72a6d3d9bcaca15452f6eb460
Luke Deen Taylor <luke@deentaylor.com>
2017-06-02 23:22:10 -0400

    Simplify listen code

--- trump-server/index.js
+++ trump-server/index.js
@@ -40,7 +40,5 @@ app.get('/:page?', (req, res) => {
     });
 });
 
-const args = process.argv.slice(2);
-const port = args.length ? args[0] : 3000;
-console.log(`Now listening at 0.0.0.0:${port}`);
-app.listen(port, '0.0.0.0');
+console.log('Now listening at http://0.0.0.0:3000');
+app.listen(3000, '0.0.0.0');
